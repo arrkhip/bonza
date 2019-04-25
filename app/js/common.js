572d3b3353da910f2f350fff21315c13095f7fdf
Arkhipov Vladislav <arkhipovvladislav@mail.ru>
2019-04-25 18:07:31 +0300

    created contacts

--- app/js/common.js
+++ app/js/common.js
@@ -2,6 +2,7 @@
 
   svg4everybody();
 
+  // slider 
   var swiper = new Swiper('.redeemed-slider', {
     slidesPerView: 3,
     spaceBetween: 30,
@@ -25,17 +26,52 @@
   });
 
 
+  // accordion
   $(function () {
     var content = $('.js-accordion-body');
     var icon = $('.js-accordion-icon');
     
     $('.js-accordion-head').on('click', function() {
-  
+      // закрытие активного пункта при клике на следующий
       // content.not($(this).next()).slideUp(500);
       $(this).next().slideToggle(400);
       $(this).find($(icon)).toggleClass('active');
 
+    });
   });
+
+
+  // yandex map
+  ymaps.ready(function(){
+    var myMap = new ymaps.Map("map", {
+      center: [54.723720, 55.943007],
+      zoom: 14,
+      controls: []
+    });
+    
+    var myGeoObject = new ymaps.GeoObject({
+      geometry: {
+        type: "Point", 
+        coordinates: [54.723720, 55.943007] 
+      }
+    });
+
+    var myPlacemark = new ymaps.Placemark([54.723720, 55.943007], {}, {
+      iconLayout: 'default#image',
+      iconImageHref: '../img/map-flag.png',
+      iconImageSize: [43, 60],
+      iconImageOffset: [-3, -42]
+    });
+
+    myMap.geoObjects.add(myPlacemark);
+    myMap.behaviors.disable('scrollZoom');
+    
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)){
+      myMap.behaviors.disable('drag');
+    }
   });
+
+
+  
 
 })(jQuery);
