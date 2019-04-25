4e057c3ea5fd1845fb4d4dda609c0c3ad2f5218c
Arkhipov Vladislav <arkhipovvladislav@mail.ru>
2019-04-25 10:27:12 +0300

    created redeemed

--- app/js/common.js
+++ app/js/common.js
@@ -2,4 +2,26 @@
 
     svg4everybody();
 
+    var swiper = new Swiper('.redeemed-slider', {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      loop: true,
+       navigation: {
+        nextEl: '.redeemed-button-next',
+        prevEl: '.redeemed-button-prev',
+      },
+      breakpoints: {
+        992: {
+          spaceBetween: 15,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        540: {
+          slidesPerView: 1,
+          spaceBetween: 20,
+        }
+      }
+    });
+
 })(jQuery);
